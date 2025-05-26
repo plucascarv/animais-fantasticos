3dd743527b21b9024fd57c97e2a2965ebf007f2b
Pedro Carvalho <plucas682@gmail.com>
2025-05-26 17:18:52 -0300

    lista acordea para faq + isolamento de elementos js

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  const activeClass = "ativo"
 
-if (tabMenu.length && tabContent.length) {
-  tabContent[0].classList.add("ativo");
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add(activeClass);
 
-  function activeTab(index) {
-  tabContent.forEach((section) => {
-    section.classList.remove("ativo");
-  });
-  tabContent[index].classList.add("ativo");
+    function activateTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove(activeClass);
+    });
+    tabContent[index].classList.add(activeClass);
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", function() {
+      activateTab(index);
+      });
+    });
+  };
 }
+initTabNav();
 
-tabMenu.forEach((item, index) => {
-  item.addEventListener("click", function() {
-    activeTab(index);
-  });
-});
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo"
+
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activateAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    };
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activateAccordion);
+    });
+  };
 }
+initAccordion();
